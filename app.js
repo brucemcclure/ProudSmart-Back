4e7dc93175f877a5a55d76af924a0f81e1f56d4f
Billy Leitch <wlei6277@gmail.com>
2019-07-11 11:44:12 +1000

    installed cors and used in app.p.js. Installed passport, created a config folder and passport file.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const morgan = require("morgan");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
