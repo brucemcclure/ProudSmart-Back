3a7fc3552bc781d1a1e7285fe3a74569fb8a75e0
Billy Leitch <wlei6277@gmail.com>
2019-07-16 12:19:44 +1000

    Created the routes and a skeleton of controller methods (only func declaration, no code) for Courses resource

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const AuthRoutes = require("./auth_routes");
 const UsersRoutes = require("./users_routes");
+const CoursesRoutes = require("./courses_routes");
 const passport = require("passport");
 
 router.get("/", (req, res) => res.send("Welcome"));
 router.use("/auth", AuthRoutes);
 router.use("/users", passport.authenticate("jwt", {session: false}), UsersRoutes);
+router.use("/courses", CoursesRoutes);
 
 module.exports = router;
