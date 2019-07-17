eaa0290cdd9ef8e9784f7995e7f397466f45ab3e
Ari <afriedgut1@gmail.com>
2019-07-17 10:38:28 +1000

    stripe works across front and back ends

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const AuthRoutes = require("./auth_routes");
 const UsersRoutes = require("./users_routes");
 const CoursesRoutes = require("./courses_routes");
+const PaymentsRoutes = require("./payments_routes");
 const passport = require("passport");
 
 router.get("/", (req, res) => res.send("Welcome"));
 router.use("/auth", AuthRoutes);
-router.use("/users", passport.authenticate("jwt", {session: false}), UsersRoutes);
+router.use(
+  "/users",
+  passport.authenticate("jwt", { session: false }),
+  UsersRoutes
+);
 router.use("/courses", CoursesRoutes);
+router.use("/payments", PaymentsRoutes);
 
 module.exports = router;
