3c1114cae947da0156a1bb7316425a343fbc1951
Joshua Tu <tjh8523@gmail.com>
2019-07-22 20:11:42 +1000

    multiple files upload can return file names array

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const UsersRoutes = require("./users_routes");
 const CoursesRoutes = require("./courses_routes");
 const PaymentsRoutes = require("./payments_routes");
 const AdminRoutes = require("./admin_routes.js");
-
+const EducatorsRoutes = require("./educators_routes");
 const ProfileImgRoutes = require("./profileImage_routes"); //Joshua
 const videoRoutes = require("./courseVideo_routes"); //Joshua
 const courseProfileImgRoutes = require("./courseProfileImg_routes"); //Joshua
@@ -17,10 +17,7 @@ const { checkRole } = require("./../middleware/auth_middleware");
 router.get("/", (req, res) => res.send("Welcome"));
 
 // routes associated with authentication (e.g. registeration / login forms)
-router.use(
-  "/auth", 
-  AuthRoutes
-);
+router.use("/auth", AuthRoutes);
 
 // routes for the user to access their account information
 router.use(
@@ -30,18 +27,12 @@ router.use(
 );
 
 // routes to access educator information
-// note educators are also users and therefore have the same document in the database. 
-// These routes use the user controller but have been seperated from the user routes for simplicity in authenitcating requests. 
-router.use(
-  "/educators",
-  EducatorsRoutes
-);
+// note educators are also users and therefore have the same document in the database.
+// These routes use the user controller but have been seperated from the user routes for simplicity in authenitcating requests.
+router.use("/educators", EducatorsRoutes);
 
 // routes to access course information
-router.use(
-  "/courses", 
-  CoursesRoutes
-);
+router.use("/courses", CoursesRoutes);
 
 // routes to accept payments
 router.use(
@@ -64,6 +55,5 @@ router.use("/image-upload", ProfileImgRoutes); //Joshua: user profile  image-upl
 router.use("/image-upload", courseProfileImgRoutes); //Joshua: course profile image-upload route
 router.use("/video-upload", videoRoutes); //single video file upload route
 router.use("/docs-upload", courseDocfilesRoutes); //multiple files upload route
-
 
 module.exports = router;
